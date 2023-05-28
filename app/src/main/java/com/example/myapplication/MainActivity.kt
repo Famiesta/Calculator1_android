aa466269d2063faf15f4fea46b12eafdb07dd3cc
TuanLy106 <lyquoctuan106@gmail.com>
2023-05-28 12:20:23 +0700

    thêm hamburger menu

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication
 
 import android.content.Intent
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -34,6 +37,9 @@ class MainActivity : AppCompatActivity() {
                 "Second Activity callback: " + data?.getSerializableExtra("key_result")
             )
         }
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
 
     override fun onSaveInstanceState(outState: Bundle) {
         outState.putSerializable("key_result",listResult)
@@ -54,6 +60,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         tv2 = findViewById(R.id.AC)
         val toolbar = findViewById<Toolbar>(R.id.my_toolbar)
+        drawer = findViewById(R.id.drawerLayout)
+        navigationView = findViewById(R.id.leftMenu)
+        navigationView.setNavigationItemSelectedListener { item->
+            when(item.itemId) {
+                R.id.action_setting -> {
+                    goToRecycleActivity()
+                }
+                else -> {
+
+                }
+            }
+            return@setNavigationItemSelectedListener true
+        }
+        //setup listener for drawer and actionbar
+        toggle = ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        //show drawe menu toggle button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
         setSupportActionBar(toolbar)
         if (savedInstanceState != null){
             ip1 = savedInstanceState.getInt("ip1")
@@ -65,13 +90,24 @@ class MainActivity : AppCompatActivity() {
         SetupUI()
     }
 
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
 
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)) {
+            return true
+        }
         when (item.itemId) {
             R.id.action_history -> {
                 goToRecycleActivity()
